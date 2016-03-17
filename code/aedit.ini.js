144f8fb91d1adb3fee55bce4d60417b4a9b9cf9e
Scriptol <webmaster@scriptol.com>
2016-03-17 10:59:49 +0100

    First release

---
+++ code/aedit.ini.js
@@ -0,0 +1,10 @@
+var config={
+    "dir": "p:/AEdit/code",
+    "path": "p:/AEdit/code/aedit.ini.js",
+    "theme": "Chaos",
+    "project": "p:/javascript-compiler/test.prj",
+    "prjList": [
+        "p:/FractalTree/tree.prj",
+        "p:/javascript-compiler/test.prj"
+    ]
+}
---
+++ demo.js
@@ -0,0 +1,32 @@
+
+function Eratosthenes(element, top)
+{
+  var all = new Uint8Array(new ArrayBuffer(top));
+  var idx = 0;
+  var prime = 3;
+  var x, j;
+
+  element.innerHTML = "1 ";
+	
+  while(prime <= top)   {
+    var flag = true;
+    for(x = 0; x < top; x++) {
+        if(all[x] == prime) 
+        {
+            flag = false;
+            break;
+        }	
+    }
+    if(flag)  {
+        element.innerHTML += prime + " ";
+        j = prime;
+        while(j <= (top / prime))  {
+            all[idx++] = prime * j;
+            j += 1;
+        }
+    }
+    prime += 2;
+  }
+  element.innerHTML += "<br>";
+  return;
+}
